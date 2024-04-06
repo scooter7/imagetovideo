cf8129f89c24811958ba50e1f21b250d60e31062
scooter7 <scooter.vineburgh@gmail.com>
2024-04-06 18:53:26 -0500

    Update imagetovideo.py

--- imagetovideo.py
+++ imagetovideo.py
@@ -1,29 +1,27 @@
 import streamlit as st
 from PIL import Image
-from moviepy.editor import VideoClip, concatenate_videoclips, AudioFileClip, CompositeAudioClip
 import numpy as np
+from moviepy.editor import ImageSequenceClip, AudioFileClip, CompositeAudioClip
 import io
 import tempfile
 
-def generate_video_from_images(image_files, fps=30, duration_per_image=3):
-    clips = []
+def generate_video_from_images(image_files, fps=1, duration_per_image=3):
+    images = []
     for img_file in image_files:
         img = Image.open(img_file)
         img = img.resize((640, 480))  # Resize image to 640x480
         img_np = np.array(img)  # Convert PIL image to numpy array
-        clip = VideoClip(lambda t: img_np, duration=duration_per_image)  # Create a clip for each image
-        clips.append(clip)
-    video = concatenate_videoclips(clips, method="compose")
-    return video
+        images.append(img_np)
+    # Create a video clip from the sequence of images
+    video = ImageSequenceClip(images, fps=fps)
+    return video.set_duration(duration_per_image * len(image_files))
 
 def add_audio_to_video(video, speech_audio=None, background_audio=None):
     if speech_audio and background_audio:
         # Use tempfile to create a temporary file on disk to bypass MoviePy's limitation
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as tmp_speech, tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as tmp_background:
-            tmp_speech.write(speech_audio.read())
-            tmp_speech.seek(0)
-            tmp_background.write(background_audio.read())
-            tmp_background.seek(0)
+        with tempfile.NamedTemporaryFile(delete=True, suffix=".mp3") as tmp_speech, tempfile.NamedTemporaryFile(delete=True, suffix=".mp3") as tmp_background:
+            tmp_speech.write(speech_audio.getvalue())
+            tmp_background.write(background_audio.getvalue())
             speech_clip = AudioFileClip(tmp_speech.name)
             background_clip = AudioFileClip(tmp_background.name).volumex(0.1)  # Reduce volume of background
             composite_audio = CompositeAudioClip([speech_clip, background_clip.set_duration(speech_clip.duration)])
@@ -39,7 +37,7 @@ uploaded_speech = st.file_uploader("Upload Speech Audio (MP3)", type=['mp3'], ac
 uploaded_background = st.file_uploader("Upload Background Audio (MP3)", type=['mp3'], accept_multiple_files=False)
 
 if st.button('Generate Video') and uploaded_images:
-    video = generate_video_from_images(uploaded_images)
+    video = generate_video_from_images(uploaded_images, duration_per_image=3)
     if uploaded_speech and uploaded_background:
         final_video = add_audio_to_video(video, uploaded_speech, uploaded_background)
     else:
